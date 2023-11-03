db4a7a10843ed84cae4805dbd208c4769bbba9a3
Jason Washburn <jason.washburn@gmail.com>
2023-11-03 17:50:17 -0500

    adds user signup and login

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,10 @@ import (
 	"errors"
 )
 
-var ErrorNoRecord = errors.New("models: no matching record found")
+var (
+	ErrorNoRecord = errors.New("models: no matching record found")
+
+	ErrInvalidCredentials = errors.New("model: invalid credentials")
+
+	ErrDuplicateEmail = errors.New("model: duplicate email")
+)
