e588533587ce769c8a936594659d6449debed5b2
Jason Washburn <jason.washburn@gmail.com>
2024-02-19 15:19:22 -0600

    Move routes/mux creation out of main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,16 +21,8 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static/", fileserver))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	logger.Info("Starting server...", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
