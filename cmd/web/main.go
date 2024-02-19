9512fb119101bf0e455f1d4e55dfe7017fdba0e1
Jason Washburn <jason.washburn@gmail.com>
2024-02-19 14:58:17 -0600

    Add application struct and logger injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,19 +7,27 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	app := application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("/static/", http.StripPrefix("/static/", fileserver))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	logger.Info("Starting server...", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
